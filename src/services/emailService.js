91243232b74c4469b0d3f4668cc454cd401ce6d8
teamp330 <teamp330@gmail.com>
2026-06-22 11:19:53 +0000

    fixed minor resend errors

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendContactEmail(contact) {
   await resend.emails.send({
-    from: "Digital Drive Tech <admin@digitaldrivetech.com>", // Later change to admin@digitaldrivetech.com
+    from: "Digital Drive Tech <onboarding@resend.dev>",
     to: process.env.COMPANY_EMAIL,
     subject: "New Contact Form Submission",
     html: `
